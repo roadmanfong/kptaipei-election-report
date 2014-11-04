f82722c60d6923e66734aadc10118c1717975784
roadman <waneblade@gmail.com>
2014-11-04 23:39:42 +0800

    with require.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,2 +1,22 @@
-
-	var map = new Map();
+requirejs.config({
+  baseUrl: 'js',
+  paths: {
+    'jquery': 'lib/jquery-2.1.1.min',
+    'underscore': 'lib/underscore-min',
+    'backbone': 'lib/backbone-min',
+  },
+  shim: {
+    'backbone': {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    'underscore': {
+        exports: '_'
+    },
+  }
+});
+requirejs([
+  'view/Map'
+],function(Map) {
+  var map = new Map();
+});
